a247b8a1aea607af08f58c192f6a71df34689b8d
Mikhail Larikov <mlarikov@gmail.com>
2022-09-24 15:17:34 +0300

    Еще одна попытка понять модули

--- keyboard/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/mlarikov/go_headfirst_packages/keyboard
-
-go 1.19
--- pass_fail/pass_fail.go
+++ pass_fail/pass_fail.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"packages/keyboard"
+)
+
+func main() {
+	fmt.Print("Enter a grade: ")
+	grade, err := keyboard.GetFloat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var status string
+	if grade > 60 {
+		status = "passing"
+	} else {
+		status = "falling"
+	}
+	fmt.Println("A grade of", grade, "is", status)
+}
